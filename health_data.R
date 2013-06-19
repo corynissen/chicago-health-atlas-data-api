d31eced892efbb2948ca1e8724386d676b19efa3
corynissen <corynissen@gmail.com>
2013-06-19 08:20:02 -0500

    change zipcode to zip

--- health_data.R
+++ health_data.R
@@ -1,13 +1,13 @@
 
 
-run <- function(zipcode=NULL, json=TRUE){
+run <- function(zip=NULL, json=TRUE){
   data_url <- "https://raw.github.com/smartchicago/chicago-atlas/master/db/import/chitrec-data.csv"
   dataFile <- tempfile()
   download.file(data_url, destfile=dataFile, method="curl")
   data <- read.csv(dataFile, stringsAsFactors=F)
   
-  if(!is.null(zipcode) & zipcode %in% data$zipcode){
-    data <- subset(data, zipcode==zipcode)
+  if(!is.null(zip) & zip %in% data$zipcode){
+    data <- subset(data, zipcode==zip)
   }
 
   if(tolower(json) %in% c(TRUE, "true", "t")){
